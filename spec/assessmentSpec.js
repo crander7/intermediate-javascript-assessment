095de4eef3b37b167e93e12f928b3c437975d400
crander7 <andersen.craigm@gmail.com>
2016-07-22 15:48:31 -0600

    done

--- spec/assessmentSpec.js
+++ spec/assessmentSpec.js
@@ -6,43 +6,43 @@ describe('grandpa', function() {
     expect(scopeArray3).toContain('grandpa', 'dad', 'son');
     expect(scopeArray4).toContain('uncle', 'cousin');
     expect(scopeArray5).toContain('son');
-  })
-})
+  });
+});
 
 describe('async', function() {
   it('should exist', function() {
-    expect(async).toEqual(jasmine.any(Function))
-  })
+    expect(async).toEqual(jasmine.any(Function));
+  });
 
   it('should not change foo before timeout', function() {
     var foo;
     async();
     expect(foo).toEqual(undefined);
-  })
+  });
 
   it('should return a promise', function() {
-    expect(async().then).toEqual(jasmine.any(Function))
-  })
+    expect(async().then).toEqual(jasmine.any(Function));
+  });
 
   it('should change foo after promise', function(done) {
     foo = undefined;
     async().then(function(res) {
       expect(foo).toEqual('bar');
       done();
-    })
-  })
-})
+    });
+  });
+});
 
 
 describe('context1', function() {
 
   var getNum = function() {
     return Math.floor(Math.random() * 100);
-  }
+  };
 
   it('should exist', function() {
     expect(context1).toEqual(jasmine.any(Function));
-  })
+  });
 
   it('should correctly assign context', function() {
     // var getNum = function() {
@@ -56,20 +56,20 @@ describe('context1', function() {
     }
     var result = context1(sum, obj, num2, num3);
     expect(result).toEqual(num + num2 + num3);
-  })
-})
+  });
+});
 
 describe('context2', function() {
   var getNum = function() {
     return Math.floor(Math.random() * 100);
-  }
+  };
 
   it('should exist', function() {
     expect(context2).toEqual(jasmine.any(Function));
-  })
+  });
 
   it('should correctly assign context', function() {
-    var nums = [getNum(), getNum(), getNum()]
+    var nums = [getNum(), getNum(), getNum()];
     console.log(nums);
     var num = getNum();
     var obj = {number: num};
@@ -80,21 +80,21 @@ describe('context2', function() {
     var result = context2(sum, obj, nums);
     expect(result).toEqual(num + nums.reduce(function(a, b) {
       return a + b;
-    }))
-  })
-})
+    }));
+  });
+});
 
 describe('context3', function() {
   var getNum = function() {
     return Math.floor(Math.random() * 100);
-  }
+  };
   it('should exist', function() {
     expect(context3).toEqual(jasmine.any(Function));
-  })
+  });
 
   it('should return a function', function() {
-    expect(context3(function() {})).toEqual(jasmine.any(Function))
-  })
+    expect(context3(function() {})).toEqual(jasmine.any(Function));
+  });
 
   it('should assign context correctly', function() {
     var obj = {
@@ -102,20 +102,20 @@ describe('context3', function() {
     };
 
     function get() {
-      return this.num
+      return this.num;
     }
 
-    expect(context3(get, obj)()).toEqual(obj.num)
-  })
-})
+    expect(context3(get, obj)()).toEqual(obj.num);
+  });
+});
 
 describe('Taco', function() {
   it('should exist', function() {
     expect(Taco).toEqual(jasmine.any(Function));
-  })
+  });
   it('should be a constructor', function() {
     expect(new Taco()).toEqual(jasmine.any(Object));
-  })
+  });
 
   it('should make a good Taco', function() {
 
@@ -128,7 +128,7 @@ describe('Taco', function() {
         shell: shell[num],
         meat: meat[num],
         veggies: veggies[num]
-      }
+      };
     }
 
     var ingredients = getIngredients();
@@ -141,34 +141,34 @@ describe('Taco', function() {
         meat: ingredients[1],
         veggies: ingredients[2]
       }
-    ))
-  })
-})
+    ));
+  });
+});
 
 describe('Burrito', function() {
   it('should exist', function() {
     expect(Burrito).toEqual(jasmine.any(Function));
-  })
+  });
 
   it('should be a constructor', function() {
     expect(new Burrito()).toEqual(jasmine.any(Object));
-  })
+  });
 
   it('should have a method called eat', function() {
     expect(new Burrito().eat).toEqual(jasmine.any(Function));
-  })
+  });
 
   it('should subtract from percentLeft', function() {
     var californiaBurrito = new Burrito();
     californiaBurrito.eat();
     californiaBurrito.eat();
     expect(californiaBurrito.percentLeft).toEqual(50);
-  })
-})
+  });
+});
 
 describe('doubler', function () {
   it('should exist', function () {
-    expect(Array.prototype.doubler).toEqual(jasmine.any(Function))
+    expect(Array.prototype.doubler).toEqual(jasmine.any(Function));
   });
 
   it('should double an array', function () {
@@ -179,10 +179,10 @@ describe('doubler', function () {
 describe('Chimichanga', function() {
   it('should exist', function() {
     expect(Chimichanga).toEqual(jasmine.any(Function));
-  })
+  });
   it('should be a constructor function', function() {
     expect(new Chimichanga()).toEqual(jasmine.any(Object));
-  })
+  });
 
   it('should make a great chimichanga', function() {
     var mmm = new Chimichanga();
@@ -191,38 +191,38 @@ describe('Chimichanga', function() {
     mmm.eat();
 
     expect(mmm.percentLeft).toEqual(60);
-  })
-})
+  });
+});
 
 describe('sentenceMachine', function() {
   it('should exist', function() {
-    expect(sentenceMachine).toEqual(jasmine.any(Function))
-  })
+    expect(sentenceMachine).toEqual(jasmine.any(Function));
+  });
 
   it('should return a function', function() {
-    expect(sentenceMachine()).toEqual(jasmine.any(Function))
-  })
+    expect(sentenceMachine()).toEqual(jasmine.any(Function));
+  });
   //TODO: randomize sentences
   it('should smash two sentences together. *HULK SMASH!*', function() {
     expect(sentenceMachine('HULK ')('SMASH')).toEqual('HULK SMASH');
-  })
-})
+  });
+});
 
 describe('subway', function() {
   beforeEach(function() {
     this.names = ['Jeremy', 'Brack', 'Brett', 'Brian', 'Jess', 'Stephen'];
     this.ingredients = ['turkey', 'bacon', 'ham', 'swiss'];
     this.name = this.names[Math.floor(Math.random() * this.names.length)];
-    this.ingredient = this.ingredients[Math.floor(Math.random() * this.ingredients.length)]
+    this.ingredient = this.ingredients[Math.floor(Math.random() * this.ingredients.length)];
     this.order = subway(this.name);
-  })
+  });
   it('should exist', function() {
     expect(subway).toEqual(jasmine.any(Function));
-  })
+  });
 
   it('should return a function', function() {
-    expect(subway("Brett")).toEqual(jasmine.any(Function))
-  })
+    expect(subway("Brett")).toEqual(jasmine.any(Function));
+  });
 
   it('should add ingredients', function() {
     var that = this;
@@ -233,31 +233,31 @@ describe('subway', function() {
           orderPerson: that.name,
           ingredients: [that.ingredient]
         }
-      ))
-  })
+      ));
+  });
 
   it('should not get orders mixed up', function() {
-    var newOrder = subway(this.names[Math.floor(Math.random() * this.names.length)])
+    var newOrder = subway(this.names[Math.floor(Math.random() * this.names.length)]);
     var originalOrder = this.order(this.ingredient);
 
     expect(newOrder).not.toEqual(originalOrder);
-  })
-})
+  });
+});
 
 describe('compareValues', function() {
   it('should exist', function() {
-    expect(compareValues).toEqual(jasmine.any(Function))
+    expect(compareValues).toEqual(jasmine.any(Function));
   });
 
   it('should find exact matches', function() {
-    expect(compareValues(7, 7)).toEqual("Exact match")
+    expect(compareValues(7, 7)).toEqual("Exact match");
   });
 
   it('should find different types matches', function() {
-    expect(compareValues(7, "7")).toEqual("Different types")
+    expect(compareValues(7, "7")).toEqual("Different types");
   });
 
   it('should find non-matches', function() {
-    expect(compareValues(7, "97")).toEqual("Different values")
+    expect(compareValues(7, "97")).toEqual("Different values");
   });
-})
+});
